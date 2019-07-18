f85d63ca06ece559476c7c03507edca35420f3e6
Alex Hawkins <alexhawkins@Alexs-MacBook-Pro.local>
2019-07-18 19:59:56 -0400

    fixed issue with results being confined to four, added minor css changes

--- wsie_app/app.js
+++ wsie_app/app.js
@@ -5,10 +5,9 @@ $(() => {
   //global variables
   const $giphyContainer = $('.giphy-container');
   const giphyArray = [];
-  // const djArray = [];
 
   // form is filled out and submitted
-  $('form').on('submit', (event) => {
+  $('form').one('submit', (event) => {
     event.preventDefault();
      //user input stored in variable
     let userInput = $('input[type="text"]').val();
@@ -17,7 +16,7 @@ $(() => {
       //pulls dj khaled gifs
       var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=DJ+KHALED&api_key=4F8CbsHhFuP1xZW9n4KwCrPEz4uCS9x0&limit=20");
       xhr.done(function(data) {
-        for (let i = 0; i < "data".length; i++) {
+        for (let i = 0; i < data.data.length; i++) {
           const $img = $('<img>').attr({'src': data['data'][i].images.original.url}).addClass('gif');
           giphyArray.push($img);
           $giphyContainer.append($img);
@@ -26,10 +25,11 @@ $(() => {
          giphyArray[0].show(); // shows first image
          showImage();
        });//end ajax
-      } else {
+    //when user inputs info
+    } else {
        var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=4F8CbsHhFuP1xZW9n4KwCrPEz4uCS9x0&limit=20");
        xhr.done(function(data) {
-         for (let i = 0; i < "data".length; i++) {
+         for (let i = 0; i < data.data.length; i++) {
            const $img = $('<img>').attr({'src': data['data'][i].images.original.url}).addClass('gif');
            giphyArray.push($img)
            $giphyContainer.append($img);
@@ -41,6 +41,7 @@ $(() => {
       $('input[type="text"]').val('') //clears input value
     } // end else statement
   }); //end listener
+
 
   //shows first image in result, appends next button
   const showImage = () => {
@@ -59,9 +60,9 @@ $(() => {
         currentGifIndex = 0;
       }
       $currentGif = giphyArray[currentGifIndex];
-      console.log($currentGif);
-      console.log(giphyArray);
-      console.log(currentGifIndex);
+      // // console.log($currentGif);
+      // // console.log(giphyArray);
+      // // console.log(currentGifIndex);
       $currentGif.show();
     })
   };
